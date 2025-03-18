997af18280ad4e5592823b6aba96d744856d987c
Divyam Chauhan <mi.divyam@gmail.com>
2025-03-18 22:33:41 +0530

    update: api endpoint updated

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,3 @@
-export const api = "http://localhost:3000/api/v1/";
+// export const api = "http://localhost:3000/api/v1/";
+export const api =
+  "https://library-management-system-tsr3.onrender.com/api/v1/";
